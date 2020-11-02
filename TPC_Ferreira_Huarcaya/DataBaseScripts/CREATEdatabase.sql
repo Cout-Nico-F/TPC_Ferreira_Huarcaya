7c2154f9eb0f3bc5f4d06b0b70f9e31ef33721ba
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-01 23:02:41 -0300

    CREATEdatabase casi completo

--- TPC_Ferreira_Huarcaya/DataBaseScripts/CREATEdatabase.sql
+++ TPC_Ferreira_Huarcaya/DataBaseScripts/CREATEdatabase.sql
@@ -37,12 +37,12 @@ create table Categorias (
 	go
 	alter table Categorias add constraint PK_Categorias primary key (ID)
 	go
-create table Nivel_Acceso (
+create table Niveles_Acceso (
 	ID smallint not null identity(1,1),
 	Descripcion varchar(100) not null
 )
 	go
-	alter table Nivel_Acceso add constraint PK_Nivel_Acceso primary key (ID)
+	alter table Niveles_Acceso add constraint PK_Niveles_Acceso primary key (ID)
 	go
 create table Colores (
 	ID smallint not null identity(1,1),
@@ -65,12 +65,16 @@ create table Relacion_Colores (
 	go
 create table Colores_X_RelacionColores (
 	ID_Color smallint not null,
-	ID_RelacionColores smallint not null
+	ID_RelacionColor smallint not null
 )
+	go
+	alter table Colores_X_RelacionColores add constraint PK_Colores_X_RelacionColores primary key (ID_Color)
 	go
 	alter table Colores_X_RelacionColores add foreign key (ID_Color) references Color(ID)
 	go
-	alter table Colores_X_RelacionColores add foreign key (ID_RelacionColores) references Relacion_Colores(ID)
+	alter table Colores_X_RelacionColores add constraint PK2_Colores_X_RelacionColores primary key (ID_Relacioncolor)
+	go
+	alter table Colores_X_RelacionColores add foreign key (ID_RelacionColor) references Relacion_Colores(ID)
 	go
 create table Estilos (
 	ID smallint not null identity(1,1),
@@ -78,6 +82,61 @@ create table Estilos (
 	ID_Seccion smallint not null
 )
 	go
-	alter table Estilos add primary key
+	alter table Estilos add constraint PK_Estilos primary key (ID)
 	go
-	alter table Estilos add foreign key
+	alter table Estilos add foreign key (ID_Seccion) references Secciones(ID)
+	go
+create table Secciones (
+	ID smallint not null identity(1,1),
+	Descripcion varchar(100) not null,
+	ID_Pedido smallint not null
+)
+	go
+	alter table Secciones add constraint PK_Secciones primary key (ID)
+	go
+	alter table Secciones add foreign key (ID_Pedido) references PedidosWebPage(ID)
+	go
+create table PedidosWebPage (
+	ID smallint not null identity(1,1),
+	ID_Usuario smallint not null,
+	ID_Categoria smallint not null
+)
+	go
+	alter table PedidosWebPage add constraint PK_Secciones primary key (ID)
+	go
+	alter table PedidosWebPage add foreign key (ID_Usuario) references Usuarios(ID)
+	go
+	alter table PedidosWebPage add foreign key (ID_Categoria) references Categorias(ID)
+create table Usuarios (
+	ID smallint not null,
+	NombreUsuario varchar(100) not null,
+	Contrasenia varchar(200) not null, --tal vez a modificar a futuro cuando veamos como cifrarla o cuanto ocupa el hash.
+	ID_Nivel smallint not null
+)
+	go
+	alter table Usuarios add constraint PK_Usuarios primary key (ID)
+	go
+	alter table Usuarios add foreign key (ID_Nivel) references Niveles_Acceso(ID)
+create table Datos_Personales (
+	ID_Usuario smallint not null,
+	Nombre_Y_Apellido varchar(200) not null,
+	Telefono_Movil int null,
+	Email varchar(200) not null,
+	Telefono_Fijo int null,
+	Fecha_Nac date not null,
+	Email_Recuperacion varchar(200) null
+)
+	go
+	alter table Datos_Personales add constraint PK_Datos_Personales primary key (ID_Usuario)
+	go
+	alter table Datos_Personales add foreign key (ID_Usuario) references Usuarios(ID)
+create table Paginas (
+	ID smallint not null identity(1,1),
+	Descripcion varchar(100) not null,
+	ID_Seccion smallint not null
+)
+	go
+	alter table Paginas add constraint PK_Paginas primary key (ID)
+	go
+	alter table Paginas add foreign key (ID_Seccion) references Secciones(ID)
+
