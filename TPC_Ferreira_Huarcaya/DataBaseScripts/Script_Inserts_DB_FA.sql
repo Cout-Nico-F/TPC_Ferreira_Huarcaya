7f38d2f461a96e5b201765c6562663a3c9db11bc
AlonsoHS21 <alonsohua20@gmail.com>
2020-11-01 22:30:07 -0300

    Avance 3: tablas casi completas
    Deje comentario en las tablas que no entiendo

--- TPC_Ferreira_Huarcaya/DataBaseScripts/Script_Inserts_DB_FA.sql
+++ TPC_Ferreira_Huarcaya/DataBaseScripts/Script_Inserts_DB_FA.sql
@@ -1,28 +1,12 @@
 
 Use [Ferreira_Huarcaya_DB];
 
---Usuario = 1/Staff = 2/Admin = 3 se me ocurrio a mi
--- O es autonumerico no tengo que agregar ID_Nivel
-insert into Usuarios(NombreUsuario,Contraseña,ID_Nivel)
-values
-('JeremiasI21','Jere123',1),
-('Lorena23','Lore321',1),
-('JazminSR1','Jazz23',1),
-('GabiF','GabFru24',1),
-('AndreaA2','Andreita15',1),
-('AlonsoHS20','AlonsohuarcayaAdmin2',3),
-('NicoFerreira1','NicoFerreAdmin1',3);
+-- tablas "terminadas" --> Secciones,Nivel Acesso,Colores,Datos_Personales,Usuarios,Paginas
+-- PedidosWebPage,Categorias,Funcionalidades_X_Paginas,Disposicion_Elementos,Elementos_X_Paginas
 
--- Si los pongo en este orden se asignaran como esta mas arriba no estoy seguro
-insert into Nivel_Acceso(Descripcion)
-values
-('Usuario'),
-('Staff'),
-('Admin');
 
-insert into Datos_Personales(Nombre_Y_Apellido,Telefono_Movil,Email,Telefono_Fijo,Fecha_Nac,Email_Recuperacion)
-values
-('Jeremias Inova','1143726710','JereIno21@hotmail.com','45232234','03/03/99','AnnaMaria44@hotmail.com');
+--                                   Tablas Maestras
+
 
 --No entendi los colores
 -- el ID es indentity(1,1)
@@ -37,6 +21,67 @@ values
 ('Gris'),
 ('Celeste'),
 ('Monocromaticos');
+
+-- Si los pongo en este orden se asignaran como esta mas arriba no estoy seguro
+insert into Nivel_Acceso(Descripcion)
+values
+('Usuario'),
+('Staff'),
+('Admin');
+
+-- el ID es indentity(1,1)
+insert into Categorias(Descripcion)
+values 
+('Ninios'),
+('Noticias'),
+('Blog'),
+('E-Commerce'),
+('Publicidad'),
+('Portfolio');
+
+--Que son las funcionalidades?
+insert into Funcionalidades(Descripcion,Precio)
+values
+('',0),
+('',100),
+('',1000),
+('',999),
+('',250),
+('',180),
+('',5000),
+('',2300);
+
+--Que son elementos?
+insert into Elementos(Descripcion,Precio)
+values
+()
+
+
+
+
+--                                  Tablas No Mestras
+
+
+
+--Usuario = 1/Staff = 2/Admin = 3 se me ocurrio a mi
+-- O es autonumerico no tengo que agregar ID_Nivel
+insert into Usuarios(NombreUsuario,Contraseña,ID_Nivel)
+values
+('JeremiasI21','Jere123',1),
+('Lorena23','Lore321',1),
+('JazminSR1','Jazz23',1),
+('GabiF','GabFru24',1),
+('AndreaA2','Andreita15',1),
+('AlonsoHS20','AlonsohuarcayaAdmin2',3),
+('NicoFerreira1','NicoFerreAdmin1',3);
+
+
+
+insert into Datos_Personales(Nombre_Y_Apellido,Telefono_Movil,Email,Telefono_Fijo,Fecha_Nac,Email_Recuperacion)
+values
+('Jeremias Inova','1143726710','JereIno21@hotmail.com','45232234','03/03/99','AnnaMaria44@hotmail.com');
+
+
 
 insert into Colores_X_RelacionColores(ID_Color,ID_RelacionColores)
 values
@@ -82,15 +127,7 @@ values
 ('Arte',10),
 ('Arte',11);
 
--- el ID es indentity(1,1)
-insert into Categorias(Descripcion)
-values 
-('Ninios'),
-('Noticias'),
-('Blog'),
-('E-Commerce'),
-('Publicidad');
-('Portfolio');
+
 
 --La verdad no se si va antes o despues
 insert into Paginas(ID_Pedido,Descripcion)
@@ -98,17 +135,48 @@ values
 (1,'Home/Detalles/Contacto'),
 (2,'Home'),
 (3,'Contacto'),
-(4,'Informacion');
+(4,'Informacion'),
+(5,'Login');
 
 insert into PedidosWebPage(ID_Usuario,ID_Pagina,ID_Categoria)
 values
 (1,1,2),
 (2,2,3),
-(3,3,5);
+(3,3,5),
+(4,4,1);
+
+
+
+insert into Funcionalidades_X_Paginas(ID_Funcionalidad,ID_Pagina)
+values
+(1,1),
+(2,1),
+(3,2),
+(4,1),
+(5,1),
+(2,2);
+
+
+insert into Disposicion_Elementos(Descripcion)
+values
+('Arriba'),
+('Abajo'),
+('Izquierda'),
+('Derecha'),
+('Centrado'),
+('Inclinado'),
+('Invertido');
+
+insert into Elementos_X_Paginas(ID_Pagina,ID_Disposicion,ID_Elemento)
+values
+(1,1,3),
+(2,1,3),
+(5,1,5),
+(2,2,2),
+(1,4,4);
 
 
 
 
--- tablas "terminadoas" --> Secciones,Nivel Acesso,Colores,Datos_Personales,Usuarios
 
 
