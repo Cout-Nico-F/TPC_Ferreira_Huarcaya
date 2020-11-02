9a5b8464322adefc7f782b9b1560a8535f5ceb42
AlonsoHS21 <alonsohua20@gmail.com>
2020-11-01 23:48:02 -0300

    Insert V1 Beta

--- TPC_Ferreira_Huarcaya/DataBaseScripts/Script_Inserts_DB_FA.sql
+++ TPC_Ferreira_Huarcaya/DataBaseScripts/Script_Inserts_DB_FA.sql
@@ -1,8 +1,5 @@
 
-Use [Ferreira_Huarcaya_DB];
-
--- tablas "terminadas" --> Secciones,Nivel Acesso,Colores,Datos_Personales,Usuarios,Paginas
--- PedidosWebPage,Categorias,Funcionalidades_X_Paginas,Disposicion_Elementos,Elementos_X_Paginas
+Use [Ferreira_Huarcaya_DB_TEST2];
 
 
 --                                   Tablas Maestras
@@ -23,7 +20,7 @@ values
 ('Monocromaticos');
 
 -- Si los pongo en este orden se asignaran como esta mas arriba no estoy seguro
-insert into Nivel_Acceso(Descripcion)
+insert into Niveles_Acceso(Descripcion)
 values
 ('Usuario'),
 ('Staff'),
@@ -40,24 +37,27 @@ values
 ('Portfolio');
 
 --Que son las funcionalidades?
-insert into Funcionalidades(Descripcion,Precio)
+insert into Funcionalidades(Descripcion,Costo)
 values
-('',0),
-('',100),
-('',1000),
-('',999),
-('',250),
-('',180),
-('',5000),
-('',2300);
+('Conexion Base de Datos',0),
+('Envio mail automatico',100),
+('Evento envial mail con boton',1000),
+('Implementacion de Google Maps',999),
+('Envio de Factura por Mail',250),
+('Evento alerta con boton',180),
+('Evento pop-up',5000);
 
 --Que son elementos?
-insert into Elementos(Descripcion,Precio)
+insert into Elementos(Descripcion,Costo)
 values
-()
-
-
-
+('Carousel',0),
+('Sticky navbar',10),
+('animation text',10),
+('Smooth Scroll',10),
+('Cover Image',0),
+('Grid',10),
+('Footer',10),
+('Header',10);
 
 --                                  Tablas No Mestras
 
@@ -65,7 +65,7 @@ values
 
 --Usuario = 1/Staff = 2/Admin = 3 se me ocurrio a mi
 -- O es autonumerico no tengo que agregar ID_Nivel
-insert into Usuarios(NombreUsuario,Contraseña,ID_Nivel)
+insert into Usuarios(NombreUsuario,Contrasenia,ID_Nivel)
 values
 ('JeremiasI21','Jere123',1),
 ('Lorena23','Lore321',1),
@@ -81,80 +81,74 @@ insert into Datos_Personales(Nombre_Y_Apellido,Telefono_Movil,Email,Telefono_Fij
 values
 ('Jeremias Inova','1143726710','JereIno21@hotmail.com','45232234','03/03/99','AnnaMaria44@hotmail.com');
 
+-- Faltan datos personales por completar
 
 
-insert into Colores_X_RelacionColores(ID_Color,ID_RelacionColores)
+
+insert into Colores_X_RelacionColores(ID_Color,ID_RelacionColor)
 values
-(1,),
-(2,),
-(3,),
-(1,),
-(1,),
-(5,),
-(6,),
+(1,1),
+(2,1),
+(3,2),
+(1,2),
+(1,2),
+(5,2),
+(6,3);
 
 -- el ID es indentity(1,1)
 insert into Relacion_Colores(Descripcion,ID_Estilo)
 values
-(,)
+('Monocromatico',1),
+('Triada',1),
+('Complementarios',1);
 
 -- el ID es indentity(1,1)
-insert into Estilos(Descripcion,ID_RelacionColores)
+insert into Estilos(Descripcion,ID_Seccion)
 values
-('Esqueumorfismo'),
-('Flat design'),
-('Material design'),
-('Maquetador/a Web'),
-('Diseniador/a Web'),
-('Barroco'),
-('Clasicismo'),
-('Surrealismo'),
-('Expesionismo'),
-('Moderno'),
-('Pixel Art');
+('Esqueumorfismo',1),
+('Flat design',1),
+('Material design',1),
+('Maquetador/a Web',2),
+('Diseniador/a Web',2),
+('Barroco',3),
+('Clasicismo',3),
+('Surrealismo',3),
+('Expesionismo',3),
+('Moderno',3),
+('Pixel Art',3);
 
-insert into Secciones(Descripcion,ID_Estilo)
+insert into Secciones(Descripcion,ID_Pedido)
 values
 ('WebPages',1),
-('WebPages',2),
-('WebPages',3),
-('Contrataciones',4),
-('Contrataciones',5),
-('Arte',6),
-('Arte',7),
-('Arte',8),
-('Arte',9),
-('Arte',10),
-('Arte',11);
-
-
+('Contrataciones',2),
+('Arte',3);
 
 --La verdad no se si va antes o despues
-insert into Paginas(ID_Pedido,Descripcion)
+insert into Paginas(ID_Seccion,Descripcion)
 values
 (1,'Home/Detalles/Contacto'),
-(2,'Home'),
-(3,'Contacto'),
-(4,'Informacion'),
-(5,'Login');
+(3,'Home'),
+(1,'Contacto'),
+(3,'Informacion'),
+(1,'Login');
 
-insert into PedidosWebPage(ID_Usuario,ID_Pagina,ID_Categoria)
+insert into PedidosWebPage(ID_Usuario,ID_Categoria)
 values
-(1,1,2),
-(2,2,3),
-(3,3,5),
-(4,4,1);
+(1,2),
+(2,3),
+(3,5),
+(4,1);
 
 
 
-insert into Funcionalidades_X_Paginas(ID_Funcionalidad,ID_Pagina)
+/*insert into Funcionalidades_X_Paginas(ID_Funcionalidad,ID_Pagina)
 values
 (1,1),
 (2,1),
 (3,2),
 (4,1),
 (5,1),
-(2,2);
+(2,2);*/
 
 
 insert into Disposicion_Elementos(Descripcion)
