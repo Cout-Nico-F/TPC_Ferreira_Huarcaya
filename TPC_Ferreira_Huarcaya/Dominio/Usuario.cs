bb6477e1e0a11b3238fa7660caf4394a8d3d7735
AlonsoHS21 <alonsohua20@gmail.com>
2020-11-04 12:57:54 -0300

    Clase Usuario

---
+++ TPC_Ferreira_Huarcaya/Dominio/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Usuario
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contrasenia { get; set; }
+
+    }
+}
