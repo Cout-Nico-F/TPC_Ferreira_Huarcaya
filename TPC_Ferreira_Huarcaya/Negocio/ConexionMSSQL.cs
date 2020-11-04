79f47524c49d41b44625ab71a82ab4574cafbdda
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-04 12:07:38 -0300

    Clase Conexion a base de datos.
    Esta clase es la que creamos para el proyecto Webforms. Se adapta perfectamente al proyecto actual. Solo tube que cambiarle la string de coneccion.

---
+++ TPC_Ferreira_Huarcaya/Negocio/ConexionMSSQL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios
+{
+    class ConexionMSSQL
+    {
+        public SqlDataReader dataReader { get; set; }
+        public SqlConnection connection { get; set; }
+        public SqlCommand command { get; set; }
+
+        public void Conectar  ()
+        {
+            connection = new SqlConnection("data source = localhost\\SQLEXPRESS01; initial catalog = Ferreira_Huarcaya_DB; integrated security = sspi");
+            command = new SqlCommand();
+            command.Connection = connection;
+            connection.Open();
+        }
+
+        public void Desconectar()
+        {
+            connection.Close();
+        }
+
+        public void SetConsulta(string consulta)
+        {
+            command.CommandText = consulta;
+        }
+
+        public SqlDataReader Leer()
+        {
+            dataReader = command.ExecuteReader();
+            dataReader.Read();
+            return dataReader;
+        }
+
+        public SqlDataReader Consulta_Rapida(string consulta)
+        {
+            Conectar();
+            SetConsulta(consulta);
+            SqlDataReader lectura = Leer();
+            Desconectar();
+            return lectura;
+        }
+    }
+}
