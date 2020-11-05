57aa317af99b14ac4148a63f5b67c2abd6217076
AlonsoHS21 <alonsohua20@gmail.com>
2020-11-05 20:30:07 -0300

    Nada trate de seguir con el Dominio y no se me ocurre como seguir

---
+++ TPC_Ferreira_Huarcaya/Dominio/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Admin
+    {
+        public int ID { get; set; }
+        public int IDUsuario { get; set; }
+
+        /*
+          Porque Admin tiene un usuario y en caso de necesitarlo 
+        desde admin puedo solicitar los datos del usuario
+
+         */
+
+        public Usuario Usuario { get; set; }
+    }
+}
--- TPC_Ferreira_Huarcaya/Dominio/Usuario.cs
+++ TPC_Ferreira_Huarcaya/Dominio/Usuario.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    class Usuario
+    public class Usuario
     {
         public int ID { get; set; }
         public string Nombre { get; set; }
