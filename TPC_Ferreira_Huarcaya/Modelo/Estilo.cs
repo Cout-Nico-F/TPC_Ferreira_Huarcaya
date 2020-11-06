7c9be1519e9bed6e9d47f37fc3460bee9456fdcb
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-06 20:36:04 -0300

    Elemento, Estilo, Pagina .cs

---
+++ TPC_Ferreira_Huarcaya/Modelo/Estilo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Estilo
+    {
+        public Int16 Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public RelacionColores RelacionColores { get; set; }//Un estilo tiene una relacion de colores.
+
+    }
+}
---
+++ TPC_Ferreira_Huarcaya/Modelo/Pagina.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Pagina
+    {
+        public Int16 Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public Int32 Costo { get; set; }
+
+        public List<Elemento> Elementos { get; set; }
+
+        public List<Funcionalidad> Funcionalidades { get; set; }
+
+    }
+}
