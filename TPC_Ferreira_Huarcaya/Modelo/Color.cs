4fe1b7f1d07e1bbb562e5dcf21903e34b38757b1
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-06 20:08:15 -0300

    Capa modeo y clase color.cs

---
+++ TPC_Ferreira_Huarcaya/Modelo/Color.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Color
+    {
+        public Int16 Id { get; set; } // Int16 para convertir el smallint de la base de datos al leerla.
+
+        public string  Descripcion { get; set; }
+
+    }
+}
