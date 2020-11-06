27d492ef7294bdd7c84a56a83bac12266c17ccbd
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-06 19:57:12 -0300

    Colores.cs

---
+++ TPC_Ferreira_Huarcaya/Modelo/Colores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Colores
+    {
+        public Int16 ID { get; set; } //Int16 segun Stackoverflow es el mejor tipo para convertir smallint. Probemos.
+        public string Descripcion { get; set; }
+
+    }
+}
