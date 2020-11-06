b104191315013cddcfcf8d3247ae0438c747d9dc
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-06 20:41:25 -0300

    Seccion, PedidoWebpage

---
+++ TPC_Ferreira_Huarcaya/Modelo/PedidoWebPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class PedidoWebPage
+    {
+        public Int16 Id { get; set; }
+
+        public string Comentarios { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public List<Seccion> Secciones { get; set; }
+    }
+}
---
+++ TPC_Ferreira_Huarcaya/Modelo/Seccion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Seccion
+    {
+        public Int16 Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public Estilo Estilo { get; set; }
+
+        public List<Pagina> Paginas { get; set; }
+    }
+}
