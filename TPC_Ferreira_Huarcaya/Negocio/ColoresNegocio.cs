1c8c6bd413686a5d224071053958ec5b6738be87
AlonsoHS21 <alonsohua20@gmail.com>
2020-11-06 20:33:45 -0300

    Agregadas las referencia en el Webform y Creado el metodo listar
    Lo hice de la forma antigua sin el uso de la clase de Acceso a datos porque me estoy viendo los videos de Maxi pasados sino me hago un lio

--- TPC_Ferreira_Huarcaya/Negocio/ColoresNegocio.cs
+++ TPC_Ferreira_Huarcaya/Negocio/ColoresNegocio.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Modelo;
+using System.Data.SqlClient;
 
 namespace Negocio
 {
     public class ColoresNegocio
     { 
+        public List<Color> listar()
+        {
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+            List<Color> lista = new List<Color>();
 
+            conexion.ConnectionString = "data source = localhost\\SQLEXPRESS01; initial catalog = Ferreira_Huarcaya_DB; integrated security = sspi";
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = "Select ID,Descripcion From Colores";
+            comando.Connection = conexion;
+
+            conexion.Open();
+            lector = comando.ExecuteReader();
+            while(lector.Read())
+            {
+                Color aux = new Color();
+                aux.Descripcion = lector.GetString(1);
+
+                lista.Add(aux);
+            }
+            conexion.Close();
+            return lista;
+        }
     }
 }
