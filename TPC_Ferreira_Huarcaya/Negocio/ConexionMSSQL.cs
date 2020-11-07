6b7d1d6eca3c0c55479fe399ed08e9e27f683e43
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-07 15:21:43 -0300

    Borre metodo repetido  y Cambie su documentacion de lugar

--- TPC_Ferreira_Huarcaya/Negocio/ConexionMSSQL.cs
+++ TPC_Ferreira_Huarcaya/Negocio/ConexionMSSQL.cs
@@ -47,25 +47,6 @@ namespace Negocios
             Desconectar();
             return lectura;
         }
-
-        /// <summary>
-        /// Consulta la base de datos y devuelve un dataset con el resultado de la consulta para ser utilizado en dropdowns u otros controles que utilizen dataset
-        /// 
-        /// </summary>
-        /// <param name="consulta"></param>
-        /// <returns></returns>
-        public DataSet ConsultaDataSet (string consulta)
-        {
-            string strconnection = "data source = localhost\\SQLEXPRESS01; initial catalog = Ferreira_Huarcaya_DB; integrated security = sspi";
-            SqlConnection connection = new SqlConnection(strconnection);
-            connection.Open();
-            SqlCommand command = new SqlCommand(consulta, connection);
-            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
-            DataSet dataSet = new DataSet();
-            dataAdapter.Fill(dataSet);
-            connection.Close();
-            return dataSet;
-        }
     
     }
 }
--- TPC_Ferreira_Huarcaya/Negocio/DropdownsNegocio.cs
+++ TPC_Ferreira_Huarcaya/Negocio/DropdownsNegocio.cs
@@ -33,6 +33,13 @@ namespace Negocio
             conexionMSSQL.Desconectar();
             return lista;
         }
+
+        /// <summary>
+        /// Consulta la base de datos y devuelve un dataset con el resultado de la consulta para ser utilizado en dropdowns u otros controles que utilizen dataset
+        /// 
+        /// </summary>
+        /// <param name="consulta"></param>
+        /// <returns></returns>
         public DataSet ConsultaDataSet(string consulta)
         {
             string strconnection = "data source = localhost\\SQLEXPRESS01; initial catalog = Ferreira_Huarcaya_DB; integrated security = sspi";
