6ff2c621df82c1c5a3d80c0aec3d52fb6041d9e3
AlonsoHS21 <alonsohua20@gmail.com>
2020-11-11 04:40:41 -0300

    Update CrearCuentaNegocio.cs

--- TPC_Ferreira_Huarcaya/Negocio/CrearCuentaNegocio.cs
+++ TPC_Ferreira_Huarcaya/Negocio/CrearCuentaNegocio.cs
@@ -1,4 +1,5 @@
-﻿using Modelo;
+﻿using System.Data.SqlClient;
+using Modelo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,37 @@ namespace Negocio
 {
     public class CrearCuentaNegocio
     {
+        public int CrearCuenta(Usuario user, DatosPersonales dat)
+        {
+            //retorna un entero porque asi esta en la DB
+            ConexionMSSQL conexion = new ConexionMSSQL();
 
+            conexion.Conectar();
+
+            string SpCrearCuenta = "Exec SP_CrearUsuario";
+
+            conexion.SetStoredProcedure(SpCrearCuenta);
+
+            conexion.AgregarParametro("@NombreUsuario", user.NombreUsuario);
+            conexion.AgregarParametro("@Contrasenia", user.Contrasenia);
+            conexion.AgregarParametro("@ID_Nivel", Convert.ToString(user.Nivel.ID));
+            conexion.AgregarParametro("@NombreApellido", dat.NombreApellido);
+            conexion.AgregarParametro("@TelefonoMovil", Convert.ToString(dat.TelefonoMovil));
+            conexion.AgregarParametro("@Email", dat.Email);
+            conexion.AgregarParametro("@Email", Convert.ToString(dat.TelefonoFijo));
+            conexion.AgregarParametro("@FechaNacimiento", Convert.ToString(dat.FechaNacimiento));
+            conexion.AgregarParametro("@EmailRecuperacion", dat.EmailRecuperacion);
+
+
+            DbDataReader lectura = conexion.Leer();
+
+            lectura.Read();
+
+            int num = lectura.GetInt32(0);
+
+            return num;
+
+
+        }
     }
 }
