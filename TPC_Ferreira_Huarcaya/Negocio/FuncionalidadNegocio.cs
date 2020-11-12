c91b281b346f660a6dbf050dc61c6a3360fd2539
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-12 14:00:06 -0300

    Pending ADD problem

---
+++ TPC_Ferreira_Huarcaya/Negocio/FuncionalidadNegocio.cs
@@ -0,0 +1,20 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    class FuncionalidadNegocio
+    {
+        public int Agregar(Funcionalidad Func)
+        {
+            ConexionMSSQL conexion = new ConexionMSSQL();
+            int rowsAfectadas = conexion.SentenciaNonQuery("insert into Funcionalidades(Descripcion, Costo) values('" + Func.Descripcion + "' , '" + Func.Costo + "')");
+            conexion.Desconectar();
+            return rowsAfectadas;
+        }
+    }
+}
--- TPC_Ferreira_Huarcaya/WebForms/ASPX/AltasModificaciones/FuncionalidadesAM.aspx.cs
+++ TPC_Ferreira_Huarcaya/WebForms/ASPX/AltasModificaciones/FuncionalidadesAM.aspx.cs
@@ -6,26 +6,32 @@ using System.Web.Profile;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Modelo;
+using Negocio;
 
 namespace WebForms.ASPX.AltasModificaciones
 {
     public partial class FuncionalidadesAM : System.Web.UI.Page
     {
         public Funcionalidad NuevaFuncionalidad { get; set; }
-        public string Placeholder { get; set; }
 
         protected void Page_Load(object sender, EventArgs e)
         {
             NuevaFuncionalidad = new Funcionalidad();
-            Placeholder = "ZZZ";
-
         }
 
         protected void btn_FuncionalidadAM_OK_Click(object sender, EventArgs e)
         {
-            
+            //setear desde el input de usuario los campos
+            //falta validar que sean correctos los datos.
             NuevaFuncionalidad.Descripcion = input_Descripcion.Value;
             NuevaFuncionalidad.Costo = Convert.ToInt32 ( input_Costo.Value ) ;
+            
+            //agregarlo a la base de datos
+            if (true)
+            {
+
+            }
+
             Response.Redirect("../Personalizar.aspx");
         }
     }
