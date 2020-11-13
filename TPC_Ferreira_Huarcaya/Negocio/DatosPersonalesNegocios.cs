994e5d4e99572edc7effa25ff77ceb8b4da87bd1
AlonsoHS21 <alonsohua20@gmail.com>
2020-11-13 17:13:55 -0300

    Cambio metodo traerDatos

--- TPC_Ferreira_Huarcaya/Negocio/DatosPersonalesNegocios.cs
+++ TPC_Ferreira_Huarcaya/Negocio/DatosPersonalesNegocios.cs
@@ -11,11 +11,12 @@ namespace Negocio
 {
     public class DatosPersonalesNegocios
     {
-        private DatosPersonales TraerDatos(DatosPersonales dat)
+        public  DatosPersonales TraerDatos(string id)
         {
-            SqlConnection Connection = new SqlConnection("data source = localhost\\SQLEXPRESS01; initial catalog = Ferreira_Huarcaya_DB; integrated security = sspi");
+            /*SqlConnection Connection = new SqlConnection("data source = localhost\\SQLEXPRESS01; initial catalog = Ferreira_Huarcaya_DB; integrated security = sspi");
             SqlCommand Command = new SqlCommand();
             Command.Connection = Connection;
+            //Select * from DatosPersonales Where id = @id_usario
             Command.Parameters.AddWithValue("@id_Usuario", dat.Usuario.ID);
             Connection.Open();
             SqlDataReader lectura = Command.ExecuteReader();
@@ -25,10 +26,23 @@ namespace Negocio
             aux.TelefonoFijo = lectura.GetInt32(2);
             aux.Email = lectura.GetString(3);
             aux.FechaNacimiento = lectura.GetDateTime(4);
-            aux.EmailRecuperacion = lectura.GetString(5);
+            aux.EmailRecuperacion = lectura.GetString(5);*/
+
+            ConexionMSSQL conexion = new ConexionMSSQL();
+            SqlDataReader lectura =  conexion.Consulta_Rapida("Select * From Datos_Personales Where id_Usuario = @id_Usuario");
+
+            conexion.AgregarParametro("@id_Usuario", id);
+
+            DatosPersonales aux = new DatosPersonales();
+            aux.NombreApellido = lectura.GetString(0);
+            aux.TelefonoMovil = lectura.GetInt32(1);
+            aux.TelefonoFijo = lectura.GetInt32(2);
+            aux.Email = lectura.GetString(3);
+            aux.FechaNacimiento = lectura.GetDateTime(4);
+            aux.EmailRecuperacion = lectura.GetString(5); 
+
 
             //nose donde poner el select
-
             return aux;
         }
     }
--- TPC_Ferreira_Huarcaya/WebForms/ASPX/EditarDatosPersonales.aspx.cs
+++ TPC_Ferreira_Huarcaya/WebForms/ASPX/EditarDatosPersonales.aspx.cs
@@ -13,10 +13,13 @@ namespace WebForms.ASPX
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string id;
             DatosPersonales dat = new DatosPersonales();
             DatosPersonalesNegocios edNeg = new DatosPersonalesNegocios();
 
-           // dat = edNeg.TraerDatos(dat);
+           // id = BuscarUsuario();
+
+            //dat = edNeg.TraerDatos(id);
 
             //Mostrarlos en el placeholder de cada textbox 
             txt_NombreApellido.Attributes.Add("placeholder", dat.NombreApellido);
@@ -26,25 +29,11 @@ namespace WebForms.ASPX
             txt_EmailRecuperacion.Attributes.Add("placeholder",dat.EmailRecuperacion);
             txt_FechaNac.Attributes.Add("placeholder",Convert.ToString(dat.FechaNacimiento));
 
-            
-
         }
 
         protected void Unnamed_Click(object sender, EventArgs e)
         {
-           /*if (validacionCampos())//va a validar si hay campos vacios
-            {
-                //para los campos vacios no hagas nada
-            }
-            else
-            {
-                //si hay algunos campos llenos cambiar
-
-            }
-
-            //en caso de que algun campo este vacio hacer que no tire error si no que no haga cambios y listo 
-
-            //Enviar datos a un insert en la DB y modificarlos*/
+          
         }
     }
 }
