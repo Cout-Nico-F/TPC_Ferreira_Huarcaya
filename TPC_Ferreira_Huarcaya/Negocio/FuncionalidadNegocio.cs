3cb3a63f20430bfebfe9237b9633062da6aac223
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-13 15:11:50 -0300

    Metodo listar para usar en la baja

--- TPC_Ferreira_Huarcaya/Negocio/FuncionalidadNegocio.cs
+++ TPC_Ferreira_Huarcaya/Negocio/FuncionalidadNegocio.cs
@@ -9,12 +9,39 @@ namespace Negocio
 {
     public class FuncionalidadNegocio
     {
+        /// <summary>
+        /// Agrega una funcionalidad a la lista en base de datos.
+        /// </summary>
+        /// <param name="Func"></param>
+        /// <returns>Devuelve la cantidad de lineas afectadas en la base. (1 si fue exitoso) </returns>
         public int Agregar(Funcionalidad Func)
         {
             ConexionMSSQL conexion = new ConexionMSSQL();
             int rowsAfectadas = conexion.SentenciaNonQuery("insert into Funcionalidades(Descripcion, Costo) values('" + Func.Descripcion + "' , '" + Func.Costo + "')");
             conexion.Desconectar();
             return rowsAfectadas;
+        }
+
+        /// <summary>
+        /// Devuelve la lista completa de funcionalidades existentes en la base de datos.
+        /// </summary>
+        /// <returns></returns>
+        public List<Funcionalidad> Listar ()
+        {
+            List<Funcionalidad> lista = new List<Funcionalidad>();
+            ConexionMSSQL conexion = new ConexionMSSQL();
+            var lectura = conexion.Consulta_Rapida("select * from funcionalidades");
+
+            while (lectura.Read())
+            {
+                Funcionalidad fun = new Funcionalidad();
+                fun.Id = lectura.GetInt16(0);
+                fun.Descripcion = lectura.GetString(1);
+                fun.Costo = lectura.GetInt32(2);
+                lista.Add(fun);
+            }
+            conexion.Desconectar();
+            return lista;
         }
     }
 }
