def4bff3f189757c4cdea82fc85a500908e12fcc
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-14 13:12:14 -0300

    Baja de funcionalidad.

--- TPC_Ferreira_Huarcaya/WebForms/ASPX/ConfirmarBajas/FuncionalidadBaja.aspx
+++ TPC_Ferreira_Huarcaya/WebForms/ASPX/ConfirmarBajas/FuncionalidadBaja.aspx
@@ -2,7 +2,10 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-
+    <div style="text-align:center">   
     <h1 class="jumbotron"> Confirmar baja de la Funcionalidad <%=Funcionalidad.Descripcion %>   </h1>
+    <h3> Esta accion eliminara la funcionalidad de la base de datos </h3>
 
+    <asp:Button ID="btn_Baja_Funcionalidad" Text="Si, deseo eliminar permanentemente esta funcionalidad"  runat="server" CssClass="btn btn-danger" OnClick="Btn_Baja_Funcionalidad_Click"/>
+    </div>
 </asp:Content>
--- TPC_Ferreira_Huarcaya/WebForms/ASPX/ConfirmarBajas/FuncionalidadBaja.aspx.cs
+++ TPC_Ferreira_Huarcaya/WebForms/ASPX/ConfirmarBajas/FuncionalidadBaja.aspx.cs
@@ -13,12 +13,29 @@ namespace WebForms.ASPX.ConfirmarBajas
     {
         protected int idRecibido;
         public Funcionalidad Funcionalidad { get; set; }
+
+        public FuncionalidadNegocio FunNegocio { get; }
+
+        public FuncionalidadBaja()
+        {
+            FunNegocio = new FuncionalidadNegocio();
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.QueryString["idFuncionalidad"] == null) 
+            {
+                Response.Redirect("../Home.aspx");//solo se puede entrar a esta pagina llegando con una id de funcionalidad por url.
+            }
             idRecibido = Convert.ToInt32(Request.QueryString["idFuncionalidad"]);
-            FuncionalidadNegocio funNegocio = new FuncionalidadNegocio();
-            var listaFuncionalidades = funNegocio.Listar();
+            
+            var listaFuncionalidades = FunNegocio.Listar();
             Funcionalidad = listaFuncionalidades.Find( f => idRecibido == f.Id );
+        }
+
+        protected void Btn_Baja_Funcionalidad_Click(object sender, EventArgs e)
+        {
+            FunNegocio.Eliminar(Funcionalidad.Id);//el metodo permite usar un if para comprobar que se pudo eliminar (1) o que no afecto ninguna row (0)
+            Response.Redirect("../Personalizar.aspx");
         }
     }
 }
