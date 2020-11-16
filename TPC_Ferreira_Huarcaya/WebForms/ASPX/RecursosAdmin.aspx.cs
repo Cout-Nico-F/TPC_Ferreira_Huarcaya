375fb638e59d952236e02a63cf04378742b27a86
AlonsoHS21 <alonsohua20@gmail.com>
2020-11-16 15:09:14 -0300

    Creado Recursos Admin para poder usar las vistas exclusivamente para los admin

---
+++ TPC_Ferreira_Huarcaya/WebForms/ASPX/RecursosAdmin.aspx.cs
@@ -0,0 +1,38 @@
+﻿using Modelo;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms.ASPX
+{
+    public partial class RecursosAdmin : System.Web.UI.Page
+    {
+
+        public Usuario Usuario { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Usuario = (Usuario)Session["usersession"];
+                if (Usuario == null)
+                {
+                    Response.Redirect("InicioSesion.aspx");
+                }
+                if(Usuario.Id_Acceso != 3)
+                {
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
