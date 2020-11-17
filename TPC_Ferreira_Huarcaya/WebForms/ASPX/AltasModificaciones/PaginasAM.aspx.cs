cf76162ee375da80c633ce90785192d867d8ccbe
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-17 13:10:55 -0300

    Revert "Modificar en proceso, todavia no funciona"
    This reverts commit b228ce37b6e8449ecaa7ef021daa87bed29c5085.

--- TPC_Ferreira_Huarcaya/WebForms/ASPX/AltasModificaciones/PaginasAM.aspx.cs
+++ TPC_Ferreira_Huarcaya/WebForms/ASPX/AltasModificaciones/PaginasAM.aspx.cs
@@ -12,34 +12,9 @@ namespace WebForms.ASPX.AltasModificaciones
     public partial class PaginasAM : System.Web.UI.Page
     {
         public Pagina Pagina { get; set; }
-        public int IdRecibido { get; set; }
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Request.QueryString["idPagina"] != null)//si llega un parametro estamos modificando
-            {
-                IdRecibido = Convert.ToInt32(Request.QueryString["idPagina"]);
-                if (IdRecibido > 0)//(0 es el id del placeholder que se llama [Paginas])
-                {
-                    if (! IsPostBack)
-                    {
-                        PaginaNegocio PagNeg = new PaginaNegocio();
-                        //modificando
-                        List<Pagina> listadoPaginasCompleto = PagNeg.Listar();
-                        Pagina = listadoPaginasCompleto.Find(p => p.ID == Convert.ToInt16(IdRecibido));
-                        //precargo el campo con los datos
-                        txtBox_Descripcion.Text = Pagina.Descripcion;
-                    }
-                }
-            }
-            else//sino estamos creando
-            {
-                if (!IsPostBack)
-                {
-                    Pagina = new Pagina();
-                    IdRecibido = -1;//id -1 lo voy a usar de bandera 
-                }
-            }
+            Pagina = new Pagina();
         }
 
         protected void Btn_PaginaAM_OK_Click(object sender, EventArgs e)
