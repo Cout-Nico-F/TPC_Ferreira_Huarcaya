3839336d86789df1bb26b4f2f479d744b86f72a8
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-17 10:57:46 -0300

    lidiando con merge errors

--- TPC_Ferreira_Huarcaya/WebForms/ASPX/AltasModificaciones/PaginasAM.aspx.cs
+++ TPC_Ferreira_Huarcaya/WebForms/ASPX/AltasModificaciones/PaginasAM.aspx.cs
@@ -11,18 +11,39 @@ namespace WebForms.ASPX.AltasModificaciones
 {
     public partial class PaginasAM : System.Web.UI.Page
     {
-        public Pagina Pagina { get; set; }
+        public int IdRecibido { get; set; }
+        public Pagina Pagina  { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            Pagina = new Pagina();
+            if (Request.QueryString["idPagina"] != null)
+            {
+                IdRecibido = Convert.ToInt32(Request.QueryString["idPagina"]);
+            }
+            else
+            {
+                Pagina = new Pagina();
+                IdRecibido = -1;
+            }
         }
-
         protected void Btn_PaginaAM_OK_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             Pagina.Descripcion = txtBox_Descripcion.Text;
             Pagina.Habilitado = true;
 
+=======
+>>>>>>> Stashed changes
             PaginaNegocio PagNeg = new PaginaNegocio();
+
+            if (IdRecibido > 0 )
+            {
+                List<Pagina> listadoPaginasCompleto = PagNeg.Listar();
+                Pagina = listadoPaginasCompleto.Find(p => p.ID == Convert.ToInt16(IdRecibido));
+                txtBox_Descripcion.Text = Pagina.Descripcion;
+            }
+
+            Pagina.Descripcion = txtBox_Descripcion.Text;
 
             if (PagNeg.Agregar(Pagina) < 1)
             {
