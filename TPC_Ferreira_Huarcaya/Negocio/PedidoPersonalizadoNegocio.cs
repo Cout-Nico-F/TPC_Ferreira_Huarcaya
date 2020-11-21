d5608dabff3baea6b80d3bda0b394ae7ccdd26a8
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-21 17:16:33 -0300

    Update PedidoPersonalizadoNegocio.cs

--- TPC_Ferreira_Huarcaya/Negocio/PedidoPersonalizadoNegocio.cs
+++ TPC_Ferreira_Huarcaya/Negocio/PedidoPersonalizadoNegocio.cs
@@ -9,13 +9,9 @@ namespace Negocio
 {
     public class PedidoPersonalizadoNegocio
     {
-        //public int AgregarPedido(PedidoPaginaPersonalizada pedido)
-        //{
-        //    ConexionMSSQL conexion = new ConexionMSSQL();
-        //    //int rowsAfectadas = conexion.SentenciaNonQuery("insert into PedidosWebPage(Id_usuario, id_paginaweb, precio, comentarios) values(" + pedido.Id_Cliente + "," + pedido.Id_WebPage + "," + pedido.Precio + ",'" + pedido.Comentarios + "')");
-        //    conexion.Desconectar();
-        //    return rowsAfectadas;
-        ////TODO: cambiar el nombre de las tablas o de las clases que confunden. (Al final del proyecto porque son cientos de lugares donde hay que cambiarlo y puede llevar semanas si algo sale mal) 
-        //}
+        public int AgregarPedido(PedidoPaginaPersonalizada pedido)
+        {
+            return int a;
+        }
     }
 }
