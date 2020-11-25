f80218afd2ca53835fd3f0dd9effd6ca74d92803
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-11-25 12:07:37 -0300

    Moviendo EmailNegocios de Modelo a Negocio

--- TPC_Ferreira_Huarcaya/Negocio/EmailNegocios.cs
+++ TPC_Ferreira_Huarcaya/Negocio/EmailNegocios.cs
@@ -5,8 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modelo;
 
-namespace Modelo
+namespace Negocio
 {
     public class EmailNegocios
     {
