e1739453f440fd5593ce21c0bec7989ed38ee600
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-12-04 15:36:48 -0300

    Regex para fecha correcta

--- TPC_Ferreira_Huarcaya/WebForms/ASPX/CrearCuenta.aspx.cs
+++ TPC_Ferreira_Huarcaya/WebForms/ASPX/CrearCuenta.aspx.cs
@@ -103,6 +103,14 @@ namespace WebForms.ASPX
                 lblFechaNaci.Text = "El campos Fecha de nacimiento esta vacio";
                 return false;
             }
+            else
+            {
+                if (! Regex.IsMatch(txtFechaNacimiento.Text, @"^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$"))
+                {
+                    lblMail.Text = "Formato de fecha incorrecta. Ingrese DIA-MES-AÑO";
+                    return false;
+                }
+            }
 
             if (txtNombreUsuario.Text == "")
             {
@@ -124,6 +132,7 @@ namespace WebForms.ASPX
                 lblMail.Text = "El formato de Correo Electronico es incorrecto ej: AlonsoHS21@hotmail.com";
                 return false;
             }
+
             
             return true;
         }
